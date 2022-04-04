6f68220dc647bbf4a8803a5111ced58abd59a406
xizana <96251460+xizana@users.noreply.github.com>
2022-04-04 23:54:01 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener("load", function(){
         function open(event){
             document.querySelector(".popup").style.display = "block";
         },
-        500
+        750
     )
 });
 document.querySelector("#close").addEventListener("click", function(){
